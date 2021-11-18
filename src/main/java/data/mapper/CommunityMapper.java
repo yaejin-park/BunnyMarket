9742788259ad9a171570383ea208f5375155281c
byuuuj <dbwjd774@naver.com>
2021-11-18 19:08:31 +0900

    byj_create : community file
    create community controller/mapper/service/sql file

---
+++ src/main/java/data/mapper/CommunityMapper.java
@@ -0,0 +1,7 @@
+package data.mapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommunityMapper {
+
+}
---
+++ src/main/java/data/service/CommunityService.java
@@ -0,0 +1,8 @@
+package data.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommunityService {
+
+}
