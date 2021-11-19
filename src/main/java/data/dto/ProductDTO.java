395b93412f83abe2d1d46a936e9ad615b4cb11a2
YAEJINPARK <pyjin805@naver.com>
2021-11-19 14:34:54 +0900

    pyj_add Product all file
    make Product file and pull main

---
+++ src/main/java/data/dto/ProductDTO.java
@@ -0,0 +1,87 @@
+package data.dto;
+
+import java.sql.Timestamp;
+
+import org.apache.ibatis.type.Alias;
+import org.springframework.web.multipart.MultipartFile;
+
+@Alias("pdto")
+public class ProductDTO {
+	private String idx;
+	private String title;
+	private String photo;
+	private String price;
+	private String content;
+	private String category;
+	private Timestamp writeday;
+	private int chatcount;
+	private int goodcount;
+	private int readcount;
+	private MultipartFile upload;
+	public String getIdx() {
+		return idx;
+	}
+	public void setIdx(String idx) {
+		this.idx = idx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Timestamp getWriteday() {
+		return writeday;
+	}
+	public void setWriteday(Timestamp writeday) {
+		this.writeday = writeday;
+	}
+	public int getChatcount() {
+		return chatcount;
+	}
+	public void setChatcount(int chatcount) {
+		this.chatcount = chatcount;
+	}
+	public int getGoodcount() {
+		return goodcount;
+	}
+	public void setGoodcount(int goodcount) {
+		this.goodcount = goodcount;
+	}
+	public int getReadcount() {
+		return readcount;
+	}
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+	public MultipartFile getUpload() {
+		return upload;
+	}
+	public void setUpload(MultipartFile upload) {
+		this.upload = upload;
+	}
+}
