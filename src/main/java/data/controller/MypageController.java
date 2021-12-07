68e718a27ce902a2f5a8125ee6d4b53e3eb148a9
HANHEEGUE <63626124+HANHEEGUE@users.noreply.github.com>
2021-12-08 00:58:24 +0900

    hhk_add:mypage member delete service upload
    mypage member delete service upload

--- src/main/java/data/controller/MypageController.java
+++ src/main/java/data/controller/MypageController.java
@@ -1,9 +1,14 @@
 package data.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import data.service.MemberService;
 
@@ -30,7 +35,34 @@ public class MypageController {
 	}
 	
 	@GetMapping("/unregister")
-	public String register() {
-		return "/mypage/unregister_member";
+	public ModelAndView register(
+		Principal principal
+		) 
+	{
+		ModelAndView mview = new ModelAndView();
+		String userId = "no";
+		String local="";
+		String[] localArr = {};
+		if(principal != null) {
+			userId = principal.getName();
+			local = memService.getLocal(principal);
+			localArr = local.split(",");
+		}
+		
+		mview.addObject("localCnt", localArr.length);
+		mview.addObject("localArr", localArr);
+		mview.setViewName("/mypage/unregister_member");
+		return mview;
+	}
+	
+	@GetMapping("/deletemember")
+	public String deleteMember(
+		Principal principal,
+		HttpServletRequest request
+		) 
+	{
+		memService.deleteMember(principal.getName());
+		request.getSession().removeAttribute("isLogin");
+		return "redirect:/logout";
 	}
 }
