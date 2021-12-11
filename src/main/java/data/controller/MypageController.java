9b500a26b63d088abb494155128524d9cba7c2f7
YiDam <44ss2210@gmail.com>
2021-12-11 17:42:52 +0900

    jid_update ProductLikefiles
    update ProductLike, delete ProductLikeController

--- src/main/java/data/controller/MypageController.java
+++ src/main/java/data/controller/MypageController.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -174,6 +173,7 @@ public class MypageController {
 	
 	@PostMapping("/member/update")
 	public String updateMember(
+		@RequestParam String type,
 		@RequestParam String email1,
 		@RequestParam String email2,
 		@RequestParam String hp1,
@@ -183,31 +183,18 @@ public class MypageController {
 		@RequestParam String zonecode,
 		@RequestParam String addr1,
 		@RequestParam String addr2,
-		MemberDTO dto,
-		Principal principal
+		MemberDTO dto
 		) 
 	{	
-		System.out.println("addrLocal=>" + addrLocal);
-		String[] localArr = memService.getLocal(principal).split(",");
-		String local = "";
-		
-		localArr[0] = addrLocal;
-		
-		for(String i:localArr) {
-			local+=i+",";
-		}
-				
-		local = local.substring(0, local.length()-1);
-		
-		
-		dto.setEmail(email1 + "@" + email2); 
+		dto.setType(type);
+		dto.setPw(encoder.encode(dto.getPw()));
+		dto.setEmail(email1 + "@" + email2);
 		dto.setHp(hp1 + "-" + hp2 + "-" + hp3);
-		dto.setLocal(local); 
+		dto.setLocal(addrLocal);
 		dto.setAddr(addr1 + "," + addr2);
-		dto.setZonecode(zonecode); 
+		dto.setZonecode(zonecode);
 		memService.updateMember(dto);
-		
-		return "redirect:../detail";
+		return "redirect:complete";
 	}
 	
 	@GetMapping("/member/deleteform")
@@ -306,49 +293,66 @@ public class MypageController {
 	}
 	@GetMapping("/productlike/list")
 	public ModelAndView productLikeList(
-			@RequestParam (defaultValue = "1") int currentPage, HttpSession session) { 
-	ModelAndView mview = new ModelAndView();
-	String id = (String)session.getAttribute("myid");
-	int totalCount = plservice.getTotalCount(id);
-	
-	//페이징 처리에 필요한 변수 선언
-	int perPage = 20;
-	int totalPage;
-	int start;
-	int perBlock = 5;
-	int startPage;
-	int endPage;
-	
-	//총 페이지 갯수 구하기
-	totalPage = totalCount/perPage+(totalCount%perPage==0?0:1);
-	//각 블럭의 시작 페이지
-	startPage = (currentPage-1)/perBlock*perBlock +1;
-	//각 블럭의 마지막 페이지
-	endPage = startPage + perBlock -1;
-	
-	if(endPage > totalPage) {
-		endPage = totalPage;
-	}
-	
-	//각 페이지에서 불러올 시작번호
-	start = (currentPage-1)*perPage;
-	
-	List<ProductDTO> list = plservice.getList(start, perPage, id);
-	
-	//각 페이지에 출력할 시작번호
-	int no = totalCount-(currentPage-1)*perPage;
-	
-	//출력에 필요한 변수들을 request에 저장
-	mview.addObject("list",list);
-	mview.addObject("startPage", startPage);
-	mview.addObject("endPage", endPage);
-	mview.addObject("totalPage", totalPage);
-	mview.addObject("no", no);
-	mview.addObject("currentPage", currentPage);
-	mview.addObject("totalCount", totalCount);
-	
-	mview.setViewName("/productlike/list");
-		  
-	return mview; 
+			@RequestParam (defaultValue = "1") int currentPage, Principal principal) { 
+		ModelAndView mview = new ModelAndView();
+		//지역 가져오기
+		String userId = "no";
+		String local = "";
+		String[] localArr = {};
+		if(principal != null) {
+			userId = principal.getName();
+			local = memService.getLocal(principal);
+			localArr = local.split(",");
+		}
+		mview.addObject("localCnt",localArr.length);
+		
+		mview.addObject("localArr",localArr);
+		String id = principal.getName();
+		int totalCount = plservice.getTotalCount(id);
+		
+		//페이징 처리에 필요한 변수 선언
+		int perPage = 20;
+		int totalPage;
+		int start;
+		int perBlock = 5;
+		int startPage;
+		int endPage;
+		
+		//총 페이지 갯수 구하기
+		totalPage = totalCount/perPage+(totalCount%perPage==0?0:1);
+		//각 블럭의 시작 페이지
+		startPage = (currentPage-1)/perBlock*perBlock +1;
+		//각 블럭의 마지막 페이지
+		endPage = startPage + perBlock -1;
+		
+		if(endPage > totalPage) {
+			endPage = totalPage;
+		}
+		
+		//각 페이지에서 불러올 시작번호
+		start = (currentPage-1)*perPage;
+		
+		List<ProductDTO> list = plservice.getList(start, perPage, id);
+		
+		//각 페이지에 출력할 시작번호
+		int no = totalCount-(currentPage-1)*perPage;
+		
+		
+		//닉네임 가져오기
+		String nick=memService.getNick(principal.getName());		
+		mview.addObject("nick", nick);
+
+		//출력에 필요한 변수들을 request에 저장
+		mview.addObject("list",list);
+		mview.addObject("startPage", startPage);
+		mview.addObject("endPage", endPage);
+		mview.addObject("totalPage", totalPage);
+		mview.addObject("no", no);
+		mview.addObject("currentPage", currentPage);
+		mview.addObject("totalCount", totalCount);
+		
+		mview.setViewName("/productlike/list");
+			  
+		return mview; 
 	}
 }
